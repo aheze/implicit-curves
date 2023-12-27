79ddb2bcf21439c535f1a1b0280a13549bffd2eb
Andrew Zheng <aheze@getfind.app>
2023-12-27 12:06:12 -0800

    Work on valuedpoint

--- QuadTree/Models/Cell.swift
+++ QuadTree/Models/Cell.swift
@@ -1,96 +1,94 @@
 //
 //  Cell.swift
 //  QuadTree
-//  
+//
 //  Created by Andrew Zheng (github.com/aheze) on 12/27/23.
 //  Copyright © 2023 Andrew Zheng. All rights reserved.
 //
 
 import SwiftUI
 
-struct Point: Equatable {
-    var x: Double
-    var y: Double
-}
 
 
-//def vertices_from_extremes(dim: int, pmin: Point, pmax: Point, fn: Func) -> list[ValuedPoint]:
+// def vertices_from_extremes(dim: int, pmin: Point, pmax: Point, fn: Func) -> list[ValuedPoint]:
 //    """Requires pmin.x ≤ pmax.x, pmin.y ≤ pmax.y"""
 //    w = pmax - pmin
 //    return [
 //        ValuedPoint(np.array([pmin[d] + (i >> d & 1) * w[d] for d in range(dim)])).calc(fn) for i in range(1 << dim)
 //    ]
 
-func verticesFromExtremes(minX: Double, maxX: Double, minY: Double, maxY: Double, function: (Point) -> Double) {
-    
+func verticesFromExtremes(
+    minX: Double,
+    maxX: Double,
+    minY: Double,
+    maxY: Double,
+    function: (Point) -> Double
+) -> Frame {
+    let bLPoint = Point(x: minX, y: minY)
+    let bRPoint = Point(x: maxX, y: minY)
+    let tLPoint = Point(x: minX, y: maxY)
+    let tRPoint = Point(x: maxX, y: maxY)
+
+    let bL = ValuedPoint(point: bLPoint, value: function(bLPoint))
+    let bR = ValuedPoint(point: bRPoint, value: function(bRPoint))
+    let tL = ValuedPoint(point: tLPoint, value: function(tLPoint))
+    let tR = ValuedPoint(point: tRPoint, value: function(tRPoint))
+
+    let frame = Frame(bL: bL, bR: bR, tL: tL, tR: tR)
+    return frame
 }
 
-struct MinimalCell {
+struct Frame {
     
-    // In 2 dimensions, vertices = [bottom-left, bottom-right, top-left, top-right] points
-//    var vertices: [CGPoint]
-    
-    var bL: Point
-    var bR: Point
-    var tL: Point
-    var tR: Point
-    
-    var vertices: [Point] {
-        [bL, bR, tL, tR]
-    }
+    var bL: ValuedPoint
+    var bR: ValuedPoint
+    var tL: ValuedPoint
+    var tR: ValuedPoint
 
-    /*
-        axis: 0 = x, 1 = y
-        direction: 0 = negative, 1 = positive
-     
-        Example: getSubcell(axis: 0, direction: 1)
-            returns the cells on the tR and bR
-     
-        Example: getSubcell(axis: 1, direction: 1)
-            returns the cells on the tL and tR
-     */
-    func getSubcell(axis: Int, direction: Int) {
-        
-        
-        
+    // In 2 dimensions, vertices = [bottom-left, bottom-right, top-left, top-right] points
+    var vertices: [ValuedPoint] {
+        [bL, bR, tL, tR]
     }
 }
 
 class Cell {
-    var minimalCell: MinimalCell
+    var frame: Frame
     var depth: Int
     var children: [Cell]
     var parent: Cell?
     var childDirection: Int
-    
-    init(minimalCell: MinimalCell, depth: Int, children: [Cell], parent: Cell?, childDirection: Int) {
-        self.minimalCell = minimalCell
+
+    init(frame: Frame, depth: Int, children: [Cell], parent: Cell?, childDirection: Int) {
+        self.frame = frame
         self.depth = depth
         self.children = children
         self.parent = parent
         self.childDirection = childDirection
     }
-    
+
     func computeChildren(function: (Point) -> Double) {
         guard children.isEmpty else {
             fatalError("Already has children")
         }
-        
-        let vertices = minimalCell.vertices
+
+        let vertices = frame.vertices
         for index in vertices.indices {
             let vertex = vertices[index]
+
+            let minX = (frame.bL.point.x + vertex.point.x) / 2
+            let maxX = (frame.tR.point.x + vertex.point.x) / 2
+            let minY = (frame.bL.point.y + vertex.point.y) / 2
+            let maxY = (frame.tR.point.y + vertex.point.y) / 2
+            let frame = verticesFromExtremes(minX: minX, maxX: maxX, minY: minY, maxY: maxY, function: function)
             
-            let minX = (minimalCell.bL.x + vertex.x) / 2
-            let maxX = (minimalCell.tR.x + vertex.x) / 2
-            let minY = (minimalCell.bL.y + vertex.y) / 2
-            let maxY = (minimalCell.tR.y + vertex.y) / 2
+            let newQuad = Cell(frame: frame, depth: depth + 1, children: [], parent: self, childDirection: index)
+            children.append(newQuad)
         }
     }
 }
 
-
-//@dataclass
-//class Cell(MinimalCell):
+// @dataclass
+// class Cell(MinimalCell):
 //    depth: int
 //    # Children go in same order: bottom-left, bottom-right, top-left, top-right
 //    children: list[Cell]
@@ -106,9 +104,8 @@ class Cell {
 //            new_quad = Cell(self.dim, vertices, self.depth + 1, [], self, i)
 //            self.children.append(new_quad)
 
-
-//@dataclass
-//class MinimalCell:
+// @dataclass
+// class MinimalCell:
 //    dim: int
 //    # In 2 dimensions, vertices = [bottom-left, bottom-right, top-left, top-right] points
 //    vertices: list[ValuedPoint]
@@ -120,3 +117,17 @@ class Cell {
 //
 //    def get_dual(self, fn: Func) -> ValuedPoint:
 //        return ValuedPoint.midpoint(self.vertices[0], self.vertices[-1], fn)
+
+
+
+/*
+    axis: 0 = x, 1 = y
+    direction: 0 = negative, 1 = positive
+
+    Example: getSubcell(axis: 0, direction: 1)
+        returns the cells on the tR and bR
+
+    Example: getSubcell(axis: 1, direction: 1)
+        returns the cells on the tL and tR
+ */
+//func getSubcell(axis: Int, direction: Int) {}
---
+++ QuadTree/Models/Point.swift
@@ -0,0 +1,48 @@
+//
+//  Point.swift
+//  QuadTree
+//
+//  Created by Andrew Zheng (github.com/aheze) on 12/27/23.
+//  Copyright © 2023 Andrew Zheng. All rights reserved.
+//
+
+import SwiftUI
+
+struct Point: Equatable {
+    var x: Double
+    var y: Double
+}
+
+// a point with the function value at the point
+struct ValuedPoint {
+    var point: Point
+    var value: Double
+
+    static func midpoint(p1: ValuedPoint, p2: ValuedPoint, function: (Point) -> Double) -> ValuedPoint {
+        let midpointX = (p1.point.x + p2.point.x) / 2
+        let midpointY = (p1.point.y + p2.point.y) / 2
+        let midpoint = Point(x: midpointX, y: midpointY)
+        let valuedPoint = ValuedPoint(point: midpoint, value: function(midpoint))
+        return valuedPoint
+    }
+
+    // Find the point on line p1--p2 with value 0
+    // linear interpolation
+    static func intersectZero(p1: ValuedPoint, p2: ValuedPoint, function: (Point) -> Double) -> ValuedPoint {
+        // get difference between values
+        let denominator = p1.value - p2.value
+
+        // Calculate the "pull" for p1 and p2
+        // if k1 is super small, then "pull" towards k2
+        let k1 = -p2.value / denominator
+        let k2 = p1.value / denominator
+
+        // Apply weights
+        let pointX = (k1 * p1.point.x) + (k2 * p2.point.x)
+        let pointY = (k1 * p1.point.y) + (k2 * p2.point.y)
+
+        let point = Point(x: pointX, y: pointY)
+        let valuedPoint = ValuedPoint(point: point, value: function(point))
+        return valuedPoint
+    }
+}
